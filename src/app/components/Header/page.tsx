e1a428460e222fd3cc1fa04c91be42b2d078c8e0
joaoleandro <new.joaoleandro@gmail.com>
2023-12-02 19:18:06 -0300

    alterado nome da pasta do active link

--- src/app/components/Header/page.tsx
+++ src/app/components/Header/page.tsx
@@ -1,25 +1,25 @@
-import Image from "next/image"
-import ActiveLink from "../active-link/page"
+import Image from "next/image";
+import ActiveLink from "../activeLink/page";
 
-export  default function Header() {
-    return(
-        <header className="w-screen h-[80px] mt-[50px] relative z-50">
-            <div className="max-w-[1920px] w-full h-full m-auto flex items-center">
-                <div className="ml-14 h-[100px] max_1024:ml-5">
-                    <Image
-                        className="mt-[17px] max_1024:w-8"
-                        src={'/images/HAMBURGUER.svg'}
-                        width={42.2}
-                        height={38.2}
-                        alt="logo"
-                    />
-                </div>
-                <div className="ml-14 flex gap-5 max_1024:ml-3 max_1024:gap-1 max_1024:mt-10">
-                    <ActiveLink name="Gallery" href={"/"}/>
-                    <ActiveLink name="About" href={"/pages/about"}/>
-                    <ActiveLink name="Contacts" href={"/pages/contacts"}/>
-                </div>
-            </div>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="w-screen h-[80px] mt-[50px] relative z-50">
+      <div className="max-w-[1920px] w-full h-full m-auto flex items-center">
+        <div className="ml-14 h-[100px] max_1024:ml-5">
+          <Image
+            className="mt-[17px] max_1024:w-8"
+            src={"/images/HAMBURGUER.svg"}
+            width={42.2}
+            height={38.2}
+            alt="logo"
+          />
+        </div>
+        <div className="ml-14 flex gap-5 max_1024:ml-3 max_1024:gap-1 max_1024:mt-10">
+          <ActiveLink name="Gallery" href={"/"} />
+          <ActiveLink name="About" href={"/pages/about"} />
+          <ActiveLink name="Contacts" href={"/pages/contacts"} />
+        </div>
+      </div>
+    </header>
+  );
 }
---
+++ src/app/components/activeLink/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export interface LinkActivo {
+    href: string;
+    name: string;
+  }
+  
+
+export default function ActiveLink({ href, name }:LinkActivo) {
+  const pathnName = usePathname()
+  const isActive = pathnName === href
+
+  return (
+    <Link href={href} className={`w-[120px] h-8 flex items-center justify-center text-white uppercase font-semibold p-3 ease-in-out duration-300  hover:bg-[#120A2A] hover:rounded-3xl
+      ${isActive ? 'border-[2px] border-solid border-[#258A2F] rounded-3xl' : ''}
+    `}>
+      {name}
+    </Link>
+  )
+}
